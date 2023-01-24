150015d588e89d93ac16b00fe32147a5295640a0
ladyvane14 <ladyvane1412@gmail.com>
2023-01-24 18:38:52 -0500

    fifth commit

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const app = new Vue({
     el: '#app',
-    data:{
+    data: {
         titulo: 'Formulario con Vue',
-        usuarios:[],
-        nuevoUsuario:''
+        usuarios: [],
+        nombres: '',
+        apellidos: '',
+        nuevoUsuario: '',
+        fecha_nac: '',
+        edad: ''
     },
-    methods:{
-        agregarUsuario: function(){
-           // console.log('diste click', this.nuevoUsuario);
-           this.usuarios.push({
-            nombre: this.nuevoUsuario
-           });
-           console.log(this.usuarios);
-            this.nuevoUsuario ='';
-            localStorage.setItem('form-vue', JSON.stringify(this.usuarios));
-    
+    methods: {
+        agregarUsuario: function () {
+            //console.log('diste click', this.nuevoUsuario);
+            this.usuarios.push({
+                nombres: this.nombres,
+                apellidos: this.apellidos,
+                usuario: this.nuevoUsuario,
+                fecha_nac: this.fecha_nac,
+                edad: this.fecha_nac,
+                clave: this.clave
+            });
+            console.log(this.usuarios);
+            this.nombres='';
+            this.apellidos='';
+            this.nuevoUsuario = '';
+            this.fecha_nac='';
+
         }
     }
-    });
+});
